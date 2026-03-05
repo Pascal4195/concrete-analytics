ed6835f0f646595577907d017d4a3591d4fb5e0d
Pascal4195 <Ndukwepraise07@gmail.com>
2026-03-05 14:04:20 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,8 +3,12 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* Glowing green Moai tiled background */}
-      <div className="moai-bg" />
+      {/* Tiled Moai emoji background */}
+      <div className="moai-bg" aria-hidden="true">
+        {Array.from({ length: 200 }).map((_, i) => (
+          <span key={i} className="moai-emoji">🗿</span>
+        ))}
+      </div>
       <div className="moai-overlay" />
       <div className="scanlines" />
       <Component {...pageProps} />
