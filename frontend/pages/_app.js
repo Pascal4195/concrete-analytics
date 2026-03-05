f8437e452fc200fbae27f969b6623e8b1f307899
Pascal4195 <Ndukwepraise07@gmail.com>
2026-03-05 13:35:48 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Glowing green Moai tiled background */}
+      <div className="moai-bg" />
+      <div className="moai-overlay" />
+      <div className="scanlines" />
+      <Component {...pageProps} />
+    </>
+  );
 }
